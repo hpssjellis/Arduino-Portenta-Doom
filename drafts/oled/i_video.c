dd23cdbf2ec3ae0a8b5b3b7f04f6d842d7b3e87f
Jeremy Ellis <keyfreemusic@gmail.com>
2026-06-03 08:49:52 -0700

    Add files via upload

--- drafts/oled/i_video.c
+++ drafts/oled/i_video.c
@@ -1,270 +1,408 @@
-// i_video.c  --  doom-oled-v004b
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
 //
-// Changes from original:
-//   - REMOVED: config.h, fcntl.h, sys/types.h  (caused nested include panic
-//              in ARM newlib toolchain; none are used by our code)
-//   - REMOVED: I_VideoBuffer_FB and double-resolution blowup (was 512 KB heap)
-//   - ADDED:   myOledInit/Row/Status/Flush calls for SSD1327 grayscale render
-//   - DG_ScreenBuffer pointed at I_VideoBuffer (valid non-null ptr, stub safe)
-//   - Game image: 320x200 -> 128x104 nearest-neighbour, 4-bit luma
-//   - Status bar: rows 106-127 (frame count + last key)
-//   - OLED throttled to ~15fps (67ms) so game loop is not dominated by SPI
+// $Id:$
 //
-// Companion file i_video_oled.cpp must be in the same src folder.
+// Copyright (C) 1993-1996 by id Software, Inc.
 //
-// Wiring: MOSI=D8, SCK=D9, CS=D7, DC=D6, RESET=-1
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// $Log:$
+//
+// DESCRIPTION:
+//	DOOM graphics stuff for X11, UNIX.
+//
+//-----------------------------------------------------------------------------
 
 static const char
 rcsid[] = "$Id: i_x.c,v 1.6 1997/02/03 22:45:10 b1 Exp $";
 
-// Doom internal headers
+#include "config.h"
 #include "v_video.h"
 #include "m_argv.h"
 #include "d_event.h"
 #include "d_main.h"
 #include "i_video.h"
 #include "z_zone.h"
+
 #include "tables.h"
 #include "doomkeys.h"
+
 #include "doomgeneric.h"
 
-// Safe standard headers only (no fcntl, no sys/types, no config.h)
 #include <stdbool.h>
 #include <stdlib.h>
+
+#include <fcntl.h>
+
 #include <stdarg.h>
-#include <stdint.h>
-#include <string.h>
-#include <limits.h>
 
-// ---------------------------------------------------------------------------
-// Forward declarations for C++ OLED wrappers in i_video_oled.cpp
-// ---------------------------------------------------------------------------
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <sys/types.h>
 
-void myOledInit(void);
-void myOledRow(int row, uint8_t* pixels);   // row 0..MY_OLED_OUT_H-1, 128 nibbles
-void myOledFlush(void);
-void myOledStatus(uint32_t frames, const char* keyLabel);
+//#define CMAP256
 
-#ifdef __cplusplus
-}
-#endif
+struct FB_BitField
+{
+	uint32_t offset;			/* beginning of bitfield	*/
+	uint32_t length;			/* length of bitfield		*/
+};
 
-// ---------------------------------------------------------------------------
-// FB info struct (kept so fb_scaling logic compiles; no fd/mmap used)
-// ---------------------------------------------------------------------------
+struct FB_ScreenInfo
+{
+	uint32_t xres;			/* visible resolution		*/
+	uint32_t yres;
+	uint32_t xres_virtual;		/* virtual resolution		*/
+	uint32_t yres_virtual;
 
-struct FB_BitField { uint32_t offset; uint32_t length; };
-
-struct FB_ScreenInfo {
-    uint32_t xres, yres, xres_virtual, yres_virtual, bits_per_pixel;
-    struct FB_BitField red, green, blue, transp;
+	uint32_t bits_per_pixel;		/* guess what			*/
+	
+							/* >1 = FOURCC			*/
+	struct FB_BitField red;		/* bitfield in s_Fb mem if true color, */
+	struct FB_BitField green;	/* else only length is significant */
+	struct FB_BitField blue;
+	struct FB_BitField transp;	/* transparency			*/
 };
 
 static struct FB_ScreenInfo s_Fb;
 int fb_scaling = 1;
-int usemouse   = 0;
+int usemouse = 0;
 
-// ---------------------------------------------------------------------------
-// Palette colour struct -- also used by i_video_oled.cpp via extern
-// ---------------------------------------------------------------------------
+struct color {
+    uint32_t b:8;
+    uint32_t g:8;
+    uint32_t r:8;
+    uint32_t a:8;
+};
 
-struct color { uint32_t b:8; uint32_t g:8; uint32_t r:8; uint32_t a:8; };
 struct color colors[256];
 
 void I_GetEvent(void);
 
-// ---------------------------------------------------------------------------
-// Video buffers
-// ---------------------------------------------------------------------------
+// The screen buffer; this is modified to draw things to the screen
 
-byte *I_VideoBuffer    = NULL;
-// I_VideoBuffer_FB REMOVED (was 640*400*2 = 512 KB, caused heap panic)
+byte *I_VideoBuffer = NULL;
+byte *I_VideoBuffer_FB = NULL;
+
+// If true, game is running as a screensaver
 
 boolean screensaver_mode = false;
+
+// Flag indicating whether the screen is currently visible:
+// when the screen isnt visible, don't render the screen
+
 boolean screenvisible;
 
-float mouse_acceleration = 2.0;
-int   mouse_threshold    = 10;
-int   usegamma           = 2;
+// Mouse acceleration
+//
+// This emulates some of the behavior of DOS mouse drivers by increasing
+// the speed when the mouse is moved fast.
+//
+// The mouse input values are input directly to the game, but when
+// the values exceed the value of mouse_threshold, they are multiplied
+// by mouse_acceleration to increase the speed.
 
-typedef struct { byte r; byte g; byte b; } col_t;
+float mouse_acceleration = 2.0;
+int mouse_threshold = 10;
+
+// Gamma correction level to use
+
+int usegamma = 2;
+
+typedef struct
+{
+	byte r;
+	byte g;
+	byte b;
+} col_t;
+
+// Palette converted to RGB565
+
 static uint16_t rgb565_palette[256];
 
-// ---------------------------------------------------------------------------
-// OLED render constants and state
-// ---------------------------------------------------------------------------
-
-#define MY_OLED_OUT_W     128
-#define MY_OLED_OUT_H     104   // game image rows (leaves 24 rows for status)
-#define MY_OLED_INTERVAL   67   // ms between OLED frames (~15 fps)
-#define MY_KEY_CLEAR_MS  2000
-
-static uint32_t myOledLastMs = 0;
-static uint32_t myFrameCount = 0;
-static char     myKeyLabel[32] = "---";
-static uint32_t myKeyLabelMs  = 0;
-
-// Call this from doomgeneric_arduino.cpp DG_GetKey to log keypresses.
-void myOledSetKey(const char* label, uint32_t nowMs)
+void cmap_to_rgb565(uint16_t * out, uint8_t * in, int in_pixels)
 {
-    int i;
-    for (i = 0; i < 31 && label[i]; i++) myKeyLabel[i] = label[i];
-    myKeyLabel[i] = '\0';
-    myKeyLabelMs  = nowMs;
-}
+    int i, j;
+    struct color c;
+    uint16_t r, g, b;
 
-// ---------------------------------------------------------------------------
-// Luma lookup table -- rebuilt on every palette change
-// ---------------------------------------------------------------------------
+    for (i = 0; i < in_pixels; i++)
+    {
+        c = colors[*in]; 
+        r = ((uint16_t)(c.r >> 3)) << 11;
+        g = ((uint16_t)(c.g >> 2)) << 5;
+        b = ((uint16_t)(c.b >> 3)) << 0;
+        *out = (r | g | b);
 
-static uint8_t myLuma[256];
-
-static void myBuildLuma(void)
-{
-    int i;
-    for (i = 0; i < 256; i++) {
-        uint32_t l = ((uint32_t)colors[i].r * 77u +
-                      (uint32_t)colors[i].g * 150u +
-                      (uint32_t)colors[i].b * 29u) >> 8;
-        myLuma[i] = (uint8_t)(l & 0xFF);
+        in++;
+        for (j = 0; j < fb_scaling; j++) {
+            *out = (r | g | b);
+            out++;
+        }
     }
 }
 
-// ---------------------------------------------------------------------------
-// Palette macros
-// ---------------------------------------------------------------------------
-
-#define GFX_RGB565(r,g,b)   ((((r&0xF8)>>3)<<11)|(((g&0xFC)>>2)<<5)|((b&0xF8)>>3))
-#define GFX_RGB565_R(c)     ((0xF800&(c))>>11)
-#define GFX_RGB565_G(c)     ((0x07E0&(c))>>5)
-#define GFX_RGB565_B(c)     (0x001F&(c))
-
-// ---------------------------------------------------------------------------
-// I_InitGraphics
-// ---------------------------------------------------------------------------
-
-void I_InitGraphics(void)
+void cmap_to_fb(uint8_t * out, uint8_t * in, int in_pixels)
 {
-    memset(&s_Fb, 0, sizeof(struct FB_ScreenInfo));
-    s_Fb.xres           = DOOMGENERIC_RESX;
-    s_Fb.yres           = DOOMGENERIC_RESY;
-    s_Fb.xres_virtual   = s_Fb.xres;
-    s_Fb.yres_virtual   = s_Fb.yres;
-    s_Fb.bits_per_pixel = 16;
-    s_Fb.blue.length    = 5;  s_Fb.blue.offset  = 0;
-    s_Fb.green.length   = 6;  s_Fb.green.offset = 5;
-    s_Fb.red.length     = 5;  s_Fb.red.offset   = 11;
+    int i, j, k;
+    struct color c;
+    uint32_t pix;
+    uint16_t r, g, b;
 
-    printf("I_InitGraphics: %dx%d fb, %dx%d doom\n",
-           s_Fb.xres, s_Fb.yres, SCREENWIDTH, SCREENHEIGHT);
+    for (i = 0; i < in_pixels; i++)
+    {
+        c = colors[*in];  /* R:8 G:8 B:8 format! */
+        r = (uint16_t)(c.r >> (8 - s_Fb.red.length));
+        g = (uint16_t)(c.g >> (8 - s_Fb.green.length));
+        b = (uint16_t)(c.b >> (8 - s_Fb.blue.length));
+        pix = r << s_Fb.red.offset;
+        pix |= g << s_Fb.green.offset;
+        pix |= b << s_Fb.blue.offset;
 
-    fb_scaling = s_Fb.xres / SCREENWIDTH;
-    if (s_Fb.yres / SCREENHEIGHT < fb_scaling)
-        fb_scaling = s_Fb.yres / SCREENHEIGHT;
+        for (k = 0; k < fb_scaling; k++) {
+            for (j = 0; j < s_Fb.bits_per_pixel/8; j++) {
+                *out = (pix >> (j*8));
+                out++;
+            }
+        }
+        in++;
+    }
+}
 
-    // 320*200 = 64 KB only; old 512 KB buffer is gone
-    I_VideoBuffer = (byte*)Z_Malloc(SCREENWIDTH * SCREENHEIGHT, PU_STATIC, NULL);
+void I_InitGraphics (void)
+{
+    int i;
 
-    // DG_ScreenBuffer must be non-null; DG_DrawFrame is a stub so safe
-    DG_ScreenBuffer = (uint32_t*)I_VideoBuffer;
+	memset(&s_Fb, 0, sizeof(struct FB_ScreenInfo));
+	s_Fb.xres = DOOMGENERIC_RESX;
+	s_Fb.yres = DOOMGENERIC_RESY;
+	s_Fb.xres_virtual = s_Fb.xres;
+	s_Fb.yres_virtual = s_Fb.yres;
+	s_Fb.bits_per_pixel = 16;
 
-    screenvisible = true;
+	s_Fb.blue.length = 5;
+	s_Fb.green.length = 6;
+	s_Fb.red.length = 5;
 
-    myOledInit();   // in i_video_oled.cpp; no-op if OLED absent
+	s_Fb.blue.offset = 0;
+	s_Fb.green.offset = 5;
+	s_Fb.red.offset = 11;
+	
+
+    printf("I_InitGraphics: framebuffer: x_res: %d, y_res: %d, x_virtual: %d, y_virtual: %d, bpp: %d\n",
+            s_Fb.xres, s_Fb.yres, s_Fb.xres_virtual, s_Fb.yres_virtual, s_Fb.bits_per_pixel);
+
+    printf("I_InitGraphics: framebuffer: RGB: %d%d%d%d, red_off: %d, green_off: %d, blue_off: %d, transp_off: %d\n",
+            s_Fb.red.length, s_Fb.green.length, s_Fb.blue.length, s_Fb.transp.length, s_Fb.red.offset, s_Fb.green.offset, s_Fb.blue.offset, s_Fb.transp.offset);
+
+    printf("I_InitGraphics: DOOM screen size: w x h: %d x %d\n", SCREENWIDTH, SCREENHEIGHT);
+
+
+    i = M_CheckParmWithArgs("-scaling", 1);
+    if (i > 0) {
+        i = atoi(myargv[i + 1]);
+        fb_scaling = i;
+        printf("I_InitGraphics: Scaling factor: %d\n", fb_scaling);
+    } else {
+        fb_scaling = s_Fb.xres / SCREENWIDTH;
+        if (s_Fb.yres / SCREENHEIGHT < fb_scaling)
+            fb_scaling = s_Fb.yres / SCREENHEIGHT;
+        printf("I_InitGraphics: Auto-scaling factor: %d\n", fb_scaling);
+    }
+
+
+    /* Allocate screen to draw to */
+	I_VideoBuffer = (byte*)Z_Malloc (SCREENWIDTH * SCREENHEIGHT, PU_STATIC, NULL);  // For DOOM to draw on
+    printf("[%s] ea_malloc %d\n", __func__, s_Fb.xres * s_Fb.yres * (s_Fb.bits_per_pixel/8));
+	I_VideoBuffer_FB = (byte*)ea_malloc(s_Fb.xres * s_Fb.yres * (s_Fb.bits_per_pixel/8));     // For a single write() syscall to fbdev
+
+	screenvisible = true;
 
     extern int I_InitInput(void);
     I_InitInput();
 }
 
-void I_ShutdownGraphics(void) { Z_Free(I_VideoBuffer); }
-void I_StartFrame(void)       {}
-void I_StartTic(void)         { I_GetEvent(); }
-void I_UpdateNoBlit(void)     {}
-
-// ---------------------------------------------------------------------------
-// I_FinishUpdate -- called by Doom every rendered frame
-// ---------------------------------------------------------------------------
-
-void I_FinishUpdate(void)
+void I_ShutdownGraphics (void)
 {
-    int oy, ox;
-    uint8_t row[MY_OLED_OUT_W];
+	Z_Free (I_VideoBuffer);
+	ea_free(I_VideoBuffer_FB);
+}
 
-    myFrameCount++;
+void I_StartFrame (void)
+{
 
-    uint32_t now = (uint32_t)millis();
-    if (now - myOledLastMs < MY_OLED_INTERVAL) {
-        DG_DrawFrame();
-        return;
+}
+
+void I_StartTic (void)
+{
+	I_GetEvent();
+}
+
+void I_UpdateNoBlit (void)
+{
+}
+
+//
+// I_FinishUpdate
+//
+
+void __attribute__((optimize("O0"))) readBuffer(uint32_t* buf, size_t len) {
+    uint32_t val;
+    for (int i=0; i<len/4; i++) {
+        val = buf[i];
     }
-    myOledLastMs = now;
+}
 
-    // Scale 320x200 -> 128x104, nearest-neighbour, palette index -> 4-bit luma
-    for (oy = 0; oy < MY_OLED_OUT_H; oy++) {
-        int sy = (oy * SCREENHEIGHT) / MY_OLED_OUT_H;
-        for (ox = 0; ox < MY_OLED_OUT_W; ox++) {
-            int     sx    = (ox * SCREENWIDTH) / MY_OLED_OUT_W;
-            uint8_t idx   = I_VideoBuffer[sy * SCREENWIDTH + sx];
-            row[ox]       = myLuma[idx] >> 4;   // 0-15
+void I_FinishUpdate (void)
+{
+
+    uint8_t* inbuf = I_VideoBuffer;
+    uint8_t* outbuf = I_VideoBuffer_FB;
+
+    // this function simply doubles the resolution
+    for (uint32_t i = 0; i < SCREENHEIGHT; i++) {
+        uint8_t* inbuf_temp = inbuf;
+        uint8_t* outbuf_temp = outbuf;
+        for (uint32_t pix = 0; pix < SCREENWIDTH; pix++) {
+            *outbuf++ = *inbuf;
+            *outbuf++ = *inbuf++;
         }
-        myOledRow(oy, row);
+        memcpy(outbuf, outbuf_temp, SCREENWIDTH * 2);
+        outbuf += SCREENWIDTH * 2;
     }
 
-    const char* kl = (now - myKeyLabelMs < MY_KEY_CLEAR_MS) ? myKeyLabel : "---";
-    myOledStatus(myFrameCount, kl);
-    myOledFlush();
+    #ifdef DEBUG_CM7_VIDEO
+    memcpy (DG_ScreenBuffer, I_VideoBuffer_FB, SCREENWIDTH * SCREENHEIGHT * 4);
+    #else
+    DG_ScreenBuffer = I_VideoBuffer_FB;
+    #endif
 
-    DG_DrawFrame();   // v001 stub
+	DG_DrawFrame();
 }
 
-// ---------------------------------------------------------------------------
-// Remaining standard functions
-// ---------------------------------------------------------------------------
-
-void I_ReadScreen(byte* scr)
+//
+// I_ReadScreen
+//
+void I_ReadScreen (byte* scr)
 {
-    memcpy(scr, I_VideoBuffer, SCREENWIDTH * SCREENHEIGHT);
+    memcpy (scr, I_VideoBuffer, SCREENWIDTH * SCREENHEIGHT);
 }
 
-void I_SetPalette(byte* palette)
+//
+// I_SetPalette
+//
+#define GFX_RGB565(r, g, b)			((((r & 0xF8) >> 3) << 11) | (((g & 0xFC) >> 2) << 5) | ((b & 0xF8) >> 3))
+#define GFX_RGB565_R(color)			((0xF800 & color) >> 11)
+#define GFX_RGB565_G(color)			((0x07E0 & color) >> 5)
+#define GFX_RGB565_B(color)			(0x001F & color)
+
+void I_SetPalette (byte* palette)
 {
-    int i;
-    for (i = 0; i < 256; i++) {
+	int i;
+	//col_t* c;
+
+	//for (i = 0; i < 256; i++)
+	//{
+	//	c = (col_t*)palette;
+
+	//	rgb565_palette[i] = GFX_RGB565(gammatable[usegamma][c->r],
+	//								   gammatable[usegamma][c->g],
+	//								   gammatable[usegamma][c->b]);
+
+	//	palette += 3;
+	//}
+    
+
+    /* performance boost:
+     * map to the right pixel format over here! */
+
+    for (i=0; i<256; ++i ) {
         colors[i].a = 0;
         colors[i].r = gammatable[usegamma][*palette++];
         colors[i].g = gammatable[usegamma][*palette++];
         colors[i].b = gammatable[usegamma][*palette++];
     }
-    myBuildLuma();
     DG_OnPaletteReload();
 }
 
-int I_GetPaletteIndex(int r, int g, int b)
+// Given an RGB value, find the closest matching palette index.
+
+int I_GetPaletteIndex (int r, int g, int b)
 {
-    int best = 0, best_diff = INT_MAX, diff, i;
+    int best, best_diff, diff;
+    int i;
     col_t color;
-    for (i = 0; i < 256; i++) {
-        color.r = GFX_RGB565_R(rgb565_palette[i]);
-        color.g = GFX_RGB565_G(rgb565_palette[i]);
-        color.b = GFX_RGB565_B(rgb565_palette[i]);
-        diff = (r-color.r)*(r-color.r)
-             + (g-color.g)*(g-color.g)
-             + (b-color.b)*(b-color.b);
-        if (diff < best_diff) { best = i; best_diff = diff; }
-        if (diff == 0) break;
+
+    printf("I_GetPaletteIndex\n");
+
+    best = 0;
+    best_diff = INT_MAX;
+
+    for (i = 0; i < 256; ++i)
+    {
+    	color.r = GFX_RGB565_R(rgb565_palette[i]);
+    	color.g = GFX_RGB565_G(rgb565_palette[i]);
+    	color.b = GFX_RGB565_B(rgb565_palette[i]);
+
+        diff = (r - color.r) * (r - color.r)
+             + (g - color.g) * (g - color.g)
+             + (b - color.b) * (b - color.b);
+
+        if (diff < best_diff)
+        {
+            best = i;
+            best_diff = diff;
+        }
+
+        if (diff == 0)
+        {
+            break;
+        }
     }
+
     return best;
 }
 
-void I_BeginRead(void)                              {}
-void I_EndRead(void)                                {}
-void I_SetWindowTitle(char *title)                  { DG_SetWindowTitle(title); }
-void I_GraphicsCheckCommandLine(void)               {}
-void I_SetGrabMouseCallback(grabmouse_callback_t func) {}
-void I_EnableLoadingDisk(void)                      {}
-void I_BindVideoVariables(void)                     {}
-void I_DisplayFPSDots(boolean dots_on)              {}
-void I_CheckIsScreensaver(void)                     {}
+void I_BeginRead (void)
+{
+}
+
+void I_EndRead (void)
+{
+}
+
+void I_SetWindowTitle (char *title)
+{
+	DG_SetWindowTitle(title);
+}
+
+void I_GraphicsCheckCommandLine (void)
+{
+}
+
+void I_SetGrabMouseCallback (grabmouse_callback_t func)
+{
+}
+
+void I_EnableLoadingDisk(void)
+{
+}
+
+void I_BindVideoVariables (void)
+{
+}
+
+void I_DisplayFPSDots (boolean dots_on)
+{
+}
+
+void I_CheckIsScreensaver (void)
+{
+}
